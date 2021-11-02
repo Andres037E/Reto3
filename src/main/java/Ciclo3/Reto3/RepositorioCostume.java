8d204d89d5555538b2a1dff4a6240b4a582381f1
Andres037E <andres.estupinan.mt@correo.usa.edu.co>
2021-11-01 21:12:34 -0500

    first commit

---
+++ src/main/java/Ciclo3/Reto3/RepositorioCostume.java
@@ -0,0 +1,37 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Ciclo3.Reto3;
+
+import java.util.List;
+import java.util.Optional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+/**
+ *
+ * @author USUARIO
+ */
+@Repository
+public class RepositorioCostume {
+    
+    @Autowired
+    private InterfaceCostume crud;
+
+    public List<Costume> getAll(){
+        return (List<Costume>) crud.findAll();
+    }
+
+    public Optional<Costume> getCostume(int id){
+        return crud.findById(id);
+    }
+
+    public Costume save(Costume costume){
+        return crud.save(costume);
+    }
+    public void delete(Costume costume){
+        crud.delete(costume);
+    }
+    
+}
